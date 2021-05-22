c497e54abbacb0bd44096ed661aa1e4c66105fad
karan-coditas <karan.pawar@coditas.com>
2021-05-23 02:10:06 +0530

    table

--- my-task/src/app/dashboard/dashboard.component.ts
+++ my-task/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserstorageService } from '../userstorage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  List : any = [];
+  constructor(private userstorageService:UserstorageService) { }
 
   ngOnInit(): void {
+    this.List = this.userstorageService.fetchFromLocalStorage();
+    console.log(this.List);
   }
 
 }
